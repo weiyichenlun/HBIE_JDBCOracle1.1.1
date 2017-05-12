7a644bfd96fdd7f6b1a64eeb384d69388a6e817b
weiyichenlun <zp472656@gmail.com>
2017-05-12 15:19:56 +0800

    add main java

---
+++ src/main/java/HAFPIS/HAFPIS_Main.java
@@ -0,0 +1,7 @@
+package HAFPIS;
+
+/**
+ * Created by ZP on 2017/5/12.
+ */
+public class HAFPIS_Main {
+}
---
+++ src/test/java/HAFPIS/HAFPIS_MainTest.java
@@ -0,0 +1,25 @@
+package HAFPIS; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* HAFPIS_Main Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ÎåÔÂ 12, 2017</pre> 
+* @version 1.0 
+*/ 
+public class HAFPIS_MainTest { 
+
+    @Before
+    public void before() throws Exception { 
+    } 
+
+    @After
+    public void after() throws Exception { 
+    } 
+
+
+} 
