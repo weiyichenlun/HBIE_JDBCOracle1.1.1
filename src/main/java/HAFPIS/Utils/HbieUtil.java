fd4dc121b913eb47bec075269366ccff7f86e29c
weiyichenlun <shmily-216504>
2017-05-15 15:52:05 +0800

    update HbieUtil

--- src/main/java/HAFPIS/Utils/HbieUtil.java
+++ src/main/java/HAFPIS/Utils/HbieUtil.java
@@ -10,6 +10,10 @@ import com.hisign.bie.thid.THIDFace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 /**
  * Created by ZP on 2017/5/12.
  */
@@ -33,7 +37,26 @@ public class HbieUtil {
         try {
             log.info("HBIEClient init begin...");
             ipAddr = ConfigUtil.getConfig("host");
-        } catch (Exception e) {
+            fpPort = Integer.parseInt(ConfigUtil.getConfig("fpPort"));
+            ppPort = Integer.parseInt(ConfigUtil.getConfig("ppPort"));
+            lppPort = Integer.parseInt(ConfigUtil.getConfig("lppPort"));
+            plpPort = Integer.parseInt(ConfigUtil.getConfig("plpPort"));
+            facePort = Integer.parseInt(ConfigUtil.getConfig("facePort"));
+            irisPort = Integer.parseInt(ConfigUtil.getConfig("irisPort"));
+
+            hbie_FP = new HBIEClient<HSFPTenFp.Record>(ipAddr, fpPort);
+            hbie_PP = new HBIEClient<HSFPFourPalm.Record>(ipAddr, ppPort);
+            hbie_LPP = new HBIEClient<HSFPLatFp.Record>(ipAddr, lppPort);
+            hbie_PLP = new HBIEClient<HSFPLatPalm.Record>(ipAddr, plpPort);
+            hbie_FACE = new HBIEClient<THIDFace.Record>(ipAddr, facePort);
+            hbie_IRIS = new HBIEClient<HSIris.Record>(ipAddr, irisPort);
+            log.info("HBIEClient init end...");
+        } catch (RemoteException e) {
+            
+        } catch (NotBoundException e) {
+
+        } catch (MalformedURLException e) {
+
         }
     }
 }
